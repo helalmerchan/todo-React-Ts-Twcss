0a815f7f48628e22a5ad3110370fea42b6efdd49
helalmerchan <helalmerchan@gmail.com>
2024-05-20 12:27:48 +0600

    complete todo app using reactTypeScriptTailwindcss

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 11,
+        title: "Like the video",
+        completed: false
+    },
+    {
+        id: 12,
+        title: "Subscribe the channel",
+        completed: false
+    },
+    {
+        id: 13,
+        title: "Leave a good Comment bellow",
+        completed: false
+    },
+];
